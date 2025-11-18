1d3c88b62327261e5232a2eb129d2cb0c6394a20
okochiyuni <i-okochiy@uniontool.co.jp>
2025-11-18 11:30:07 +0900

    テストオーダーの変更

--- tests/test_cal_pi.py
+++ tests/test_cal_pi.py
@@ -2,19 +2,19 @@ import numpy as np
 from main import Rectangle, Trapezoid, Simpson
 
 def test_rectangle_pi_accuracy():
-    N = 100_000
+    N = 10_000
     pi_est = Rectangle(N)
     error = abs(pi_est - np.pi)
     assert error < 1e-4, f"Rectangle 誤差が大きい: {error}"
 
 def test_trapezoid_pi_accuracy():
-    N = 100_000
+    N = 10_000
     pi_est = Trapezoid(N)
     error = abs(pi_est - np.pi)
     assert error < 1e-6, f"Trapezoid 誤差が大きい: {error}"
 
 def test_simpson_pi_accuracy():
-    N = 100_000  # 偶数にする
+    N = 10_000  # 偶数にする
     pi_est = Simpson(N)
     error = abs(pi_est - np.pi)
     assert error < 1e-7, f"Simpson 誤差が大きい: {error}"
